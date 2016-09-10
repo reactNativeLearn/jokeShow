135be724e01a36dab83a274d2f87b0289203108a
shijh <shijh@jingoal.com>
2016-09-10 15:54:33 +0800

    热部署update加入

--- index.android.js
+++ index.android.js
@@ -9,10 +9,40 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  ToastAndroid,
+  Alert,
+  Vibration,
 } from 'react-native';
 
+import codePush from "react-native-code-push";
+
 class jokeShow extends Component {
+  componentDidMount(){
+    codePush.checkForUpdate()
+    .then( (update) =>{
+        if( !update ){
+          Alert.alert(
+            'Alert Title',
+            "app是最新版了",
+            [
+              {text: 'OK', onPress: () => ToastAndroid.show('OK Pressed!', ToastAndroid.LONG)},
+            ]
+          )
+        }else {
+          Alert.alert(
+            'Alert Title',
+            "有更新哦",
+            [
+              {text: 'OK', onPress: () => ToastAndroid.show('OK Pressed!', ToastAndroid.LONG)},
+            ]
+          )
+        }
+    });
+    // codePush.sync();
+    // codePush.restartApp()
+    // ToastAndroid.show('OK Pressed!', ToastAndroid.LONG)
+  }
   render() {
     return (
       <View style={styles.container}>
@@ -24,7 +54,8 @@ class jokeShow extends Component {
         </Text>
         <Text style={styles.instructions}>
           Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
+          Shake or press menu button for dev menu,{'\n'}
+          dshjfhasjdfhajkshdfjkashjkd
         </Text>
       </View>
     );
@@ -47,6 +78,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
+    color: 'red',
   },
 });
 
